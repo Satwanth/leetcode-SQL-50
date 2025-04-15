eb5feb153c2d98b8ee8363dcd61e504c686cb050
Satwanth Naidu Vardhineedi <svard3@unh.newhaven.edu>
2025-04-14 20:30:21 -0400

    Add files via upload

---
+++ SQL50_Q38(LC.626).sql
@@ -0,0 +1,62 @@
+/*
+Table: Seat
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key (unique value) column for this table.
+Each row of this table indicates the name and the ID of a student.
+The ID sequence always starts from 1 and increments continuously.
+ 
+
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Seat table:
++----+---------+
+| id | student |
++----+---------+
+| 1  | Abbot   |
+| 2  | Doris   |
+| 3  | Emerson |
+| 4  | Green   |
+| 5  | Jeames  |
++----+---------+
+Output: 
++----+---------+
+| id | student |
++----+---------+
+| 1  | Doris   |
+| 2  | Abbot   |
+| 3  | Green   |
+| 4  | Emerson |
+| 5  | Jeames  |
++----+---------+
+Explanation: 
+Note that if the number of students is odd, there is no need to change the last one's seat.
+*/
+
+with prevNext as (select *,
+lag(student) over(order by id) as prev,
+lead(student) over(order by id) as next,
+count(student) over() as num_of_students
+from Seat)
+
+select id,
+case
+    when id = num_of_students and id % 2 != 0 then student
+    when id % 2 != 0 then next
+    else prev
+    end student
+from prevNext;
